e85a1dd77b47cd7037ff2e132a6aba75e3c8b491
Jacob Huffman <Coppylobbies1@gmail.com>
2019-08-12 22:34:50 -0700

    saving work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+function ContentCard(props) {
+  return <div className="ContentCard">{props.children}</div>;
+}
+
+function Section(props) {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <p>{props.content}</p>
+    </div>
+  );
+}
+
+var contentArray = [
+  {
+    title: "lorem",
+    content:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit"
+  },
+  {
+    title: "lorem",
+    content:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit"
+  }
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="Header">WELCOME TO</h1>
+        <h2 id="test">Test Site</h2>
       </header>
+      <body>
+        {contentArray.map(function (obj) {
+          return (
+            <ContentCard>
+              <Section title={obj.title} content={obj.content} />
+            </ContentCard>
+          );
+        })}
+      </body>
     </div>
   );
 }
